2c2bf0b9352f11be398c3b30764a2692a8c963df
aryansingh144 <aryansingh7910@gmail.com>
2024-10-08 19:39:43 +0530

    additional features

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -18,7 +18,10 @@ router.route("/register").post(
     registerUser
 )
 
-router.route("./login").post(loginUser)
+router.route("/login").post(loginUser)
+
+
+router.route("/logout").post(verifyJWT,logoutUser)
 
 
 export default router
